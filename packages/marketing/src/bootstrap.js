a62b5cd2e581b52e0af14ca9fdddf7e26622e8ac
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2021-12-07 13:07:05 +0200

    navigation between microfrontend apps

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount fn to start the app
-const mount = (el) => {
-	ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+	const history = defaultHistory || createMemoryHistory();
+
+	onNavigate && history.listen(onNavigate);
+
+	ReactDOM.render(<App history={history} />, el);
+
+	return {
+		onParentNavigate({ pathname: nextPathname }) {
+			const { pathname } = history.location;
+
+			if (pathname !== nextPathname) {
+				history.push(nextPathname);
+			}
+		}
+	};
 };
 
 // If we are in the development and isolation
@@ -12,7 +27,7 @@ const mount = (el) => {
 if (process.env.NODE_ENV === 'development') {
 	const devRoot = document.getElementById('_marketing-dev-root');
 
-	devRoot && mount(devRoot);
+	devRoot && mount(devRoot, { defaultHistory: createBrowserHistory() });
 }
 
 // We are running through container
